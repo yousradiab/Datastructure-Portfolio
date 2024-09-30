1d7f9f4062f43fdf842c90ce4c181c9e801b2b69
Yousra Diab <yousserah@gmail.com>
2024-09-30 15:47:51 +0200

    Update script.js

--- Queue/GridRunner/script.js
+++ Queue/GridRunner/script.js
@@ -8,6 +8,8 @@ window.addEventListener("load", start);
 function start() {
   console.log(`Javascript kører`);
 
+  document.addEventListener("keydown", keyDown);
+
   // start ticking
   tick();
 }
@@ -15,17 +17,89 @@ function start() {
 function tick() {
   // setup next tick
   setTimeout(tick, 500);
+  
+     for (const part of queue) {
+  writeToCell(part.row, part.col, 0);
+  }
 
-  // TODO: Do stuff
+  const head = {
+    row: queue[queue.length-1].row,
+    col: queue[queue.length-1].col
+  }
+
+switch (direction) {
+  case "left":
+    head.col--;
+    if (head.col < 0) {
+      head.col = 9;
+    }
+    break;
+    case "right":
+      head.col++;
+      if (head.col > 9) {
+        head.col = 0;
+      }
+      break;
+    case "up":
+      head.row--;
+      if (head.row < 0) {
+        head.row = 9;
+      }
+      break;
+    case "down":
+      head.row++;
+      if (head.row > 9) {
+        head.row = 0;
+      }
+    }
+
+
+    //sæt nyt element ind i hovedet (slutningen)
+    queue.push(head);
+    
+    queue.shift();
+
+     for (const part of queue) {
+  writeToCell(part.row, part.col, 1);
+  }
+
+
 
   // display the model in full
   displayBoard();
+  
+
+}
+
+function keyDown(event) {
+  switch (event.key) {
+    case "ArrowLeft":
+      direction = "left";
+      break;
+    case "ArrowRight":
+      direction = "right";
+      break;
+    case "ArrowUp":
+      direction = "up";
+      break;
+    case "ArrowDown":
+      direction = "down";
+      break;
+  }
 }
 
 // #endregion controller
 
 // ****** MODEL ******
 // #region model
+let direction = "right";
+const head = {row: 5, col: 5};
+const queue = [
+  {row: 5, col: 7},
+  {row:5, col:6},
+  {row:5, col:5}
+];
+
 const model = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -42,6 +116,7 @@ const model = [
 function writeToCell(row, col, value) {
   model[row][col] = value;
 }
+
 
 function readFromCell(row, col) {
   return model[row][col];
